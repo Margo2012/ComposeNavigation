4ce018be8fd7eb04ef759335b1785d7c4881084a
user <novikova.rita@yandex.ru>
2024-08-01 02:58:23 +0300

    панель навигации

--- app/src/main/java/com/example/sky/MainActivity.kt
+++ app/src/main/java/com/example/sky/MainActivity.kt
@@ -1,36 +1,109 @@
 package com.example.sky
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
-import com.example.sky.navigation.ScreenA
-import com.example.sky.navigation.ScreenB
-import com.example.sky.ui.screens.HomeScreen
-import com.example.sky.ui.screens.NextScreen
 import com.example.sky.ui.theme.SkyTheme
 
+data class BottomNavItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+)
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SkyTheme {
+                val items = listOf(
+                    BottomNavItem(
+                        title = "Авиабилеты",
+                        selectedIcon = Icons.Filled.Send,
+                        unselectedIcon = Icons.Outlined.Send
+                    ),
+                    BottomNavItem(
+                        title = "Отели",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home
+                    ),
+                    BottomNavItem(
+                        title = "Короче",
+                        selectedIcon = Icons.Filled.LocationOn,
+                        unselectedIcon = Icons.Outlined.LocationOn
+                    ),
+                    BottomNavItem(
+                        title = "Поделиться",
+                        selectedIcon = Icons.Filled.Notifications,
+                        unselectedIcon = Icons.Outlined.Notifications
+                    ),
+                    BottomNavItem(
+                        title = "Профиль",
+                        selectedIcon = Icons.Filled.Person,
+                        unselectedIcon = Icons.Outlined.Person
+                    )
+                )
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = { selectedItemIndex = index },
+                                        label = { Text(text = item.title)},
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (selectedItemIndex == index)
+                                                    item.selectedIcon
+                                                else
+                                                    item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
+                                        })
+                                }
+                            }
+                        }
+                    ) {
+
+                    }
+                    /*val navController = rememberNavController()
                     NavHost(
                         navController = navController,
                         startDestination = ScreenA
@@ -46,7 +119,9 @@ class MainActivity : ComponentActivity() {
                             val args = it.toRoute<ScreenB>()
                             NextScreen(args.name, args.age)
                         }
-                    }
+                    }*/
+
+
                 }
             }
         }
