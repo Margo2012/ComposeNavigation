a58f48e536a978bead7c3017feeb6f558b27d02a
user <novikova.rita@yandex.ru>
2024-08-01 21:02:43 +0300

    заголовок

--- app/src/main/java/com/example/sky/MainActivity.kt
+++ app/src/main/java/com/example/sky/MainActivity.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
@@ -23,14 +26,16 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sky.ui.screens.DepartureField
 import com.example.sky.ui.theme.SkyTheme
 
@@ -102,7 +107,19 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     ) {
-                        DepartureField(modifier = Modifier)
+                        Column {
+                            Text(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth()
+                                    .align(Alignment.CenterHorizontally),
+                                text = "Поиск дешевых\nавиабилетов",
+                                fontSize = 22.sp,
+                                textAlign = TextAlign.Center,
+                                maxLines = 2
+                            )
+                            DepartureField(modifier = Modifier)
+                        }
 
                     }
                     /*val navController = rememberNavController()
@@ -130,18 +147,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SkyTheme {
-        Greeting("Android")
-    }
-}
